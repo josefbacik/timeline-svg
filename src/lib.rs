3249976b0f02b728db5a9852c9397a13ecb07257
Josef Bacik <josef@toxicpanda.com>
2025-01-16 11:43:54 -0500

    making it more of a heatmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,12 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{Result, Write};
-use std::collections::HashMap;
+use std::time::Duration;
 
 use rand::prelude::*;
 use svg::node::element::path::Data;
 use svg::node::element::{Group, Line, Path, Rectangle, Text};
+use alphanumeric_sort::compare_str;
 
 const COLORS: &'static [&'static str] = &[
     "blue",
@@ -17,7 +19,6 @@ const COLORS: &'static [&'static str] = &[
     "pink",
     "cyan",
     "brown",
-    "black",
     "gray",
     "magenta",
     "olive",
@@ -28,31 +29,33 @@ const COLORS: &'static [&'static str] = &[
     "aqua",
     "silver",
     "fuchsia",
-    "white",
 ];
 
+#[derive(Debug, PartialEq, Clone)]
 pub enum TimeUnit {
     Nanoseconds,
     Microseconds,
     Milliseconds,
     Seconds,
-    Minutes,
-    Hours,
-    Days,
 }
 
 pub struct Timeline {
     start_time: u64,
     end_time: u64,
-    events: Vec<Event>,
+    events: HashMap<String, Vec<Event>>,
     triggers: Vec<Trigger>,
     units: TimeUnit,
+    major_unit: TimeUnit,
     row_height: u64,
-    column_width: u64,
+    width_per_unit: f64,
     row_padding: u64,
     column_padding: u64,
+    width_per_major_unit: f64,
+    min_elapsed_time: u64,
+    time_scale: u64,
 }
 
+#[derive(Debug, Clone)]
 struct Event {
     name: String,
     start_time: u64,
@@ -71,18 +74,56 @@ impl Default for Timeline {
         Timeline {
             start_time: u64::MAX,
             end_time: 0,
-            events: Vec::new(),
+            events: HashMap::new(),
             triggers: Vec::new(),
             units: TimeUnit::Nanoseconds,
-            row_height: 20,
-            column_width: 200,
+            major_unit: TimeUnit::Seconds,
+            row_height: 100,
+            width_per_unit: 10.0,
             row_padding: 1,
             column_padding: 0,
+            width_per_major_unit: 1000.0,
+            min_elapsed_time: u64::MAX,
+            time_scale: 1,
         }
     }
 }
 
 impl Timeline {
+    /*
+    fn calculate_time_scale(&mut self) {
+        let mut units = self.units.clone();
+        let mut min = self.min_elapsed_time;
+        self.time_scale = 1;
+        if units == TimeUnit::Nanoseconds {
+            if min < 1_000 {
+                self.major_unit = TimeUnit::Microseconds;
+                return;
+            }
+            min /= 1_000;
+            self.time_scale *= 1_000;
+            units = TimeUnit::Microseconds;
+        }
+        if units == TimeUnit::Microseconds {
+            if min < 1_000 {
+                self.major_unit = TimeUnit::Milliseconds;
+                return;
+            }
+            min /= 1_000;
+            self.time_scale *= 1_000;
+            units = TimeUnit::Milliseconds;
+        }
+        if units == TimeUnit::Milliseconds {
+            if min < 1_000 {
+                self.major_unit = TimeUnit::Seconds;
+                return;
+            }
+            self.time_scale *= 1_000;
+            self.major_unit = TimeUnit::Seconds;
+        }
+    }
+    */
+
     /// Add an event to the timeline
     ///
     /// This function adds an event to the timeline. Events do not need to be added in
@@ -101,7 +142,17 @@ impl Timeline {
         if event.end_time > self.end_time {
             self.end_time = event.end_time;
         }
-        self.events.push(event);
+        let elapsed = event.end_time - event.start_time;
+        if elapsed < self.min_elapsed_time {
+            self.min_elapsed_time = elapsed;
+            //self.calculate_time_scale();
+        }
+        match self.events.get_mut(&event.location) {
+            Some(events) => events.push(event),
+            None => {
+                self.events.insert(event.location.clone(), vec![event]);
+            }
+        }
     }
 
     /// Add a trigger to the timeline
@@ -120,6 +171,7 @@ impl Timeline {
     /// use timeline_svg::Timeline;
     ///
     /// let mut timeline = Timeline::default();
+    /// timeline.set_units(timeline_svg::TimeUnit::Milliseconds);
     /// timeline.add_event("Process A".to_string(), 0, 1, "CPU 0".to_string());
     /// timeline.add_event("Process B".to_string(), 1, 2, "CPU 1".to_string());
     /// timeline.add_trigger("CPU 0".to_string(), "CPU 1".to_string(), 1);
@@ -137,7 +189,8 @@ impl Timeline {
         if trigger.time > self.end_time {
             self.end_time = trigger.time;
         }
-        self.triggers.push(trigger);
+
+        //        self.triggers.push(trigger);
     }
 
     /// Save the timeline to a file
@@ -158,73 +211,108 @@ impl Timeline {
         self.units = units;
     }
 
-    fn make_timeline_box(&self) -> Group {
-        let num_secs = self.end_time - self.start_time;
-        let width = num_secs * self.column_width;
-        let big_tick = self.row_height / 2;
-        let small_tick = self.row_height / 4;
+    fn time_to_major_unit(&self, time: Duration) -> u64 {
+        match self.major_unit {
+            TimeUnit::Nanoseconds => time.as_nanos() as u64,
+            TimeUnit::Microseconds => time.as_micros() as u64,
+            TimeUnit::Milliseconds => time.as_millis() as u64,
+            TimeUnit::Seconds => time.as_secs(),
+        }
+    }
 
+    fn make_timeline_box(&self) -> Group {
+        let row_height = 20;
+        let total_duration = Duration::from_nanos(self.end_time - self.start_time);
+        let width = total_duration.as_millis() as f64 / 100.0;
+        let width_per_major = 100.0;
+        let big_tick = row_height / 2;
+        let small_tick = row_height / 4;
+
+        println!(
+            "Making timeline box, width {}, min_elapsed_time {}",
+            width, self.min_elapsed_time
+        );
         let mut g = Group::new();
         g = g.add(
             Line::new()
                 .set("x1", 0)
-                .set("y1", self.row_height)
+                .set("y1", row_height)
                 .set("x2", width)
-                .set("y2", self.row_height)
+                .set("y2", row_height)
                 .set("stroke", "black")
                 .set("stroke-width", 1),
         );
 
-        for i in 0..num_secs {
+        let mut cur_time = Duration::from_nanos(0);
+        while cur_time < total_duration {
             // Big tick for our start
+            let x = cur_time.as_secs() as f64 * width_per_major;
             g = g
                 .add(
                     Line::new()
-                        .set("x1", i * self.column_width)
-                        .set("y1", self.row_height)
-                        .set("x2", i * self.column_width)
-                        .set("y2", self.row_height - big_tick)
+                        .set("x1", x)
+                        .set("y1", row_height)
+                        .set("x2", x)
+                        .set("y2", row_height - big_tick)
                         .set("stroke", "black")
                         .set("stroke-width", 1),
                 )
                 .add(
-                    Text::new(format!("{}", i))
-                        .set("x", i * self.column_width)
-                        .set("y", self.row_height - big_tick)
+                    Text::new(format!("{}", cur_time.as_secs()))
+                        .set("x", x)
+                        .set("y", row_height - big_tick)
                         .set("font-size", 10)
                         .set("fill", "black"),
                 );
 
             // Small ticks for the middle parts
-            for tick in 1..9 {
-                let x = i * self.column_width + (self.column_width / 10) * tick;
+            for tick in 1..10 {
+                let x = cur_time.as_secs() as f64 * width_per_major
+                    + (width_per_major / 10.0) * tick as f64;
                 let line = Line::new()
                     .set("x1", x)
-                    .set("y1", self.row_height)
+                    .set("y1", row_height)
                     .set("x2", x)
-                    .set("y2", self.row_height - small_tick)
+                    .set("y2", row_height - small_tick)
                     .set("stroke", "black")
                     .set("stroke-width", 1);
                 g = g.add(line);
             }
+            cur_time += Duration::from_secs(1);
         }
         g
     }
 
+    fn scale_time(&self, time: Duration) -> f64 {
+        let start_time = Duration::from_nanos(self.start_time);
+        let duration = time - start_time;
+        if duration < Duration::from_millis(1) {
+            return 0.0;
+        }
+
+        duration.as_millis() as f64 * self.width_per_unit
+    }
+
     // Calculate the x position of a time
-    fn time_x(&self, time: u64) -> u64 {
+    fn time_x(&self, time: u64) -> f64 {
         let padding = if time == self.start_time {
             0
         } else {
             self.column_padding
         };
-        (time - self.start_time) * self.column_width + padding
+        let time = time - self.start_time;
+        ((time as f64 / self.min_elapsed_time as f64) * self.width_per_unit) + padding as f64
+    }
+
+    // Calculate the width of a time duration
+    fn time_width(&self, start_time: u64, end_time: u64) -> f64 {
+        (end_time - start_time) as f64 / self.min_elapsed_time as f64 * self.width_per_unit
     }
 
     // Calculate the y position of a category
     fn category_y(&self, category: &String, categories: &Vec<String>) -> u64 {
         let y = categories.iter().position(|c| c == category).unwrap() as u64;
-        (y + 1) * self.row_height + self.row_padding
+        y * self.row_height + self.row_padding + 20
     }
 
     /// Write the SVG of the timeline to a writer
@@ -233,47 +321,103 @@ impl Timeline {
     /// on each category, with triggers connecting the events. Random colors are used for the
     /// events, and the colors are kept consistent with the same event.
     pub fn write(&self, writer: &mut dyn Write) -> Result<()> {
-        let mut categories: Vec<String> = self
-            .events
-            .iter()
-            .map(|event| event.location.clone())
-            .collect::<Vec<String>>();
-        categories.sort();
+        println!("Sorting categories events {}", self.events.len());
         let mut colormap: HashMap<String, String> = HashMap::new();
-
-        let num_secs = self.end_time - self.start_time;
-        let width = num_secs * self.column_width;
-        let height = (categories.len() as u64) * self.row_height + self.row_height;
+        let mut categories: Vec<String> = self.events.clone().into_keys().collect();
+        categories.sort_by(|a, b| compare_str(a, b));
+        println!("eh??");
+        let width = (self.end_time - self.start_time) as f64 / self.min_elapsed_time as f64
+            * self.width_per_unit;
+        let height = (categories.len() as u64) * self.row_height + 10;
 
         let mut doc = svg::Document::new()
             .set("width", width)
             .set("height", height)
             .add(self.make_timeline_box());
 
-        for event in &self.events {
-            let color = colormap
-                .entry(event.name.clone())
-                .or_insert_with(|| {
-                    let mut rng = rand::thread_rng();
-                    COLORS[rng.gen_range(0..COLORS.len())].to_string()
-                });
-            let x = self.time_x(event.start_time);
-            let y = self.category_y(&event.location, &categories);
-            let rect = Rectangle::new()
-                .set("x", x)
-                .set("y", y)
-                .set("width", self.column_width)
-                .set("height", self.row_height)
-                .set("fill", (*color).clone());
-            let label = Text::new(event.name.clone())
-                .set("x", x)
-                .set("y", y + 10)
-                .set("font-size", 10)
-                .set("fill", "black");
-            let g = Group::new().add(rect).add(label);
-            doc = doc.add(g);
+        let mut max_len = 0;
+        for category in categories.iter() {
+            doc = doc.add(
+                Text::new(category.clone())
+                    .set("x", 0)
+                    .set("y", self.category_y(category, &categories) + 10)
+                    .set("font-size", 10)
+                    .set("fill", "black"),
+            );
+            if category.len() > max_len {
+                max_len = category.len();
+            }
         }
 
+        println!("Adding events {}", self.events.len());
+        let mut first = true;
+        for category in &categories {
+            let events = self.events.get(category).unwrap();
+            if events.len() == 0 {
+                continue;
+            }
+            println!("Events for category {} {}", category, events.len());
+            let mut events = events.clone();
+            events.sort_by(|a, b| a.start_time.cmp(&b.start_time));
+            let mut busy_time = Duration::new(0, 0);
+            let mut cur_x = max_len * 10;
+            let mut cutoff = Duration::from_nanos(self.start_time) + Duration::from_millis(10);
+            for event in events {
+                let mut start_time = Duration::from_nanos(event.start_time);
+                let end_time = Duration::from_nanos(event.end_time);
+                while start_time >= cutoff {
+                    if busy_time > Duration::from_nanos(0) {
+                        println!("Busy time is {:?}", busy_time.as_millis());
+                        let y1 = self.category_y(&category, &categories) + self.row_height;
+                        let y2 = y1 - (busy_time.as_millis() * 10) as u64;
+                        let line = Line::new()
+                            .set("x1", cur_x)
+                            .set("y1", y1)
+                            .set("x2", cur_x)
+                            .set("y2", y2)
+                            .set("stroke", "black")
+                            .set("stroke-width", 1);
+                        doc = doc.add(line);
+                    }
+                    busy_time = Duration::from_nanos(0);
+                    cutoff += Duration::from_millis(10);
+                    cur_x += 1;
+                }
+
+                while end_time >= cutoff {
+                    if start_time < cutoff {
+                        busy_time += cutoff - start_time;
+                    }
+                    if busy_time > Duration::from_nanos(0) {
+                        println!("Busy time is {:?}", busy_time.as_millis());
+                        let y1 = self.category_y(&category, &categories) + self.row_height;
+                        let y2 = y1 - (busy_time.as_millis() * 10) as u64;
+                        let line = Line::new()
+                            .set("x1", cur_x)
+                            .set("y1", y1)
+                            .set("x2", cur_x)
+                            .set("y2", y2)
+                            .set("stroke", "black")
+                            .set("stroke-width", 1);
+                        doc = doc.add(line);
+                    }
+                    start_time = cutoff;
+                    busy_time = Duration::from_nanos(0);
+                    while start_time >= cutoff {
+                        cutoff += Duration::from_millis(10);
+                        cur_x += 1;
+                    }
+                }
+                busy_time += end_time - start_time;
+
+                if first {
+                    println!("Event {:?}, startime {}", event, self.start_time);
+                    first = false;
+                }
+            }
+            println!("cur_x is {}", cur_x);
+        }
+        println!("Adding triggers {}", self.triggers.len());
         for trigger in &self.triggers {
             let x = self.time_x(trigger.time);
             let start_y = self.category_y(&trigger.start_location, &categories);
@@ -286,6 +430,8 @@ impl Timeline {
                 .set("fill", "none");
             doc = doc.add(path);
         }
+        println!("Writing to file");
+
         writer.write_all(doc.to_string().as_bytes())
     }
 }
@@ -329,15 +475,16 @@ mod tests {
     #[test]
     fn test_offsets() {
         let mut timeline = Timeline::default();
+        timeline.set_units(TimeUnit::Seconds);
         timeline.add_event("Event 1".to_string(), 1, 2, "Location 1".to_string());
         timeline.add_event("Event 2".to_string(), 3, 4, "Location 2".to_string());
         timeline.add_trigger("Location 1".to_string(), "Location 2".to_string(), 1);
         let categories = vec!["Location 1".to_string(), "Location 2".to_string()];
 
-        assert_eq!(timeline.time_x(1), 0);
-        assert_eq!(timeline.time_x(2), 200);
-        assert_eq!(timeline.time_x(3), 400);
-        assert_eq!(timeline.time_x(4), 600);
+        assert_eq!(timeline.time_x(1), 0.0);
+        assert_eq!(timeline.time_x(2), 200.0);
+        assert_eq!(timeline.time_x(3), 400.0);
+        assert_eq!(timeline.time_x(4), 600.0);
         assert_eq!(
             timeline.category_y(&"Location 1".to_string(), &categories),
             21
@@ -346,5 +493,16 @@ mod tests {
             timeline.category_y(&"Location 2".to_string(), &categories),
             41
         );
+    }
+
+    #[test]
+    fn test_widths() {
+        let mut timeline = Timeline::default();
+        timeline.set_units(TimeUnit::Milliseconds);
+        timeline.add_event("Event 1".to_string(), 0, 100, "Location 1".to_string());
+        timeline.add_event("Event 2".to_string(), 100, 200, "Location 2".to_string());
+
+        assert_eq!(timeline.time_width(0, 100), 20.0);
+        assert_eq!(timeline.time_width(100, 200), 20.0);
     }
 }
